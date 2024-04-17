a252043920892da79d16fdb36b62f2629dac8b25
eliry213 <eliry213@student.liu.se>
2024-04-17 18:43:34 +0200

    trying to use solid ui

--- sup/src/App.tsx
+++ sup/src/App.tsx
@@ -1,11 +1,13 @@
 import type { ParentComponent } from 'solid-js'
 import { Suspense } from 'solid-js'
 import { A } from '@solidjs/router'
+import { Button } from './components/ui/button'
 
 
 const App: ParentComponent = (props) => (
   <>
     <A href="/"> Startsida </A>
+    <Button>hejsan</Button>
     <A href="/schedule"> Schema </A>
     <A href="/apply"> Ansökan </A>
     <Suspense>{props.children}</Suspense>
